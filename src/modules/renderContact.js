ec6bf17c6271d69d05a3d2303d3405c2eef37a08
David Hillman <dhillman1989@gmail.com>
2021-01-28 18:01:54 +0000

    added contact and menu pages

--- src/modules/renderContact.js
+++ src/modules/renderContact.js
@@ -2,8 +2,35 @@ const renderContact = () => {
   const mainContent = document.querySelector(".mainContent");
 
   mainContent.innerHTML = `
-    
-      <h3>CONTACT</h3>
+    <div class="contact__image">
+        <img src="images/phone.svg">
+    </div>
+    <div class="contact__text">
+        <h4>Phone</h4>
+        <p>All Bookings: 01509 554 334</p>
+        
+        <p>Branch: 01509 554 334</p>
+        <p>Head Office: 01509 554 334</p>
+    </div>
+    <div class="contact__text">
+        <h4>Email</h4>
+        <p>RiverBoat@Getinn.com
+        <p>For general enquiries only. For all bookings and reservations please use the Bookings Number</p>
+    </div>
+    <div class="contact__text">
+        <h4>Social Media</h4>
+        <ul>
+            <li>
+                <i class="fab fa-facebook-square"></i><span>Facebook</span>
+            </li>
+            <li>
+                <i class="fab fa-twitter-square"></i><span>Twitter</span>
+            </li>
+            <li>
+            <i class="fab fa-instagram-square"></i></i><span>Instagram</span>
+            </li>
+        </ul>
+    </div>
   `;
 };
 
--- src/modules/renderMenu.js
+++ src/modules/renderMenu.js
@@ -1,7 +1,19 @@
 const renderMenu = () => {
   const mainContent = document.querySelector(".mainContent");
 
-  mainContent.innerHTML = `MENU`;
+  mainContent.innerHTML = `
+    <div class="menu__text">
+        <h2>Our Menu</h2>
+        <p>
+            This is a sample of our main menu, but we have specials and fresh recipes coming out of our kitchen all the time.
+            Make sure you book your table to experience it for yourself, and see what all the locals are talking about.
+        </p>
+        <img class="menu__promo" src="./images/25percent.png"/>
+        <p>
+        Call us now and make your booking to receive 25% off your entire bill.* (Subject to terms and conditions)
+        </p>
+    </div>
+    <img class="menu__image" src="./images/menu.jpg">`;
 };
 
 export default renderMenu;
